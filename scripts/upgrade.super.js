811bce57b21d3268540d0c4b944ecb210bcf7811
Farhad <farhad.szd@gmail.com>
2024-03-12 13:25:39 +0400

    initial commit

---
+++ scripts/upgrade.super.js
@@ -0,0 +1,20 @@
+const { ethers, upgrades } = require("hardhat");
+
+async function main() {
+  const Super = await ethers.getContractFactory("StakingSuperPool");
+  const sp = await upgrades.deployProxy(Super, []);
+
+  console.log("Super deployed to:", sp.target);
+
+  const SuperUp = await ethers.getContractFactory("StakingSuperPoolUp");
+  const spUp = await upgrades.upgradeProxy(sp.target, SuperUp);
+
+  console.log("Super upgraded to:", spUp.target);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
